df5b9ea4f417c414cc80e66cfbe9803e089fb43f
Aleksander Lukashov <lukashov784@gmail.com>
2025-08-31 14:19:59 +0700

    fifth: bug fixes

--- quotes/services/set_seen.py
+++ quotes/services/set_seen.py
@@ -4,11 +4,10 @@ from quotes.models import Quote
 
 
 def set_seen(request, pk):
-    """Прибавляем просмотр к цитате и добавляем по ключу seen_quote pk этой цитаты для защиты от накрутки"""
-    seen = request.session.get('seen_quotes', set())
-    if not isinstance(seen, set):
-        seen = set(seen)
+    """Прибавляем просмотр к цитате и добавляем pk в сессию, чтобы не засчитывать повторно"""
+    seen = set(request.session.get('seen_quotes', []))
     if str(pk) not in seen:
         Quote.objects.filter(pk=pk).update(views=F('views') + 1)
         seen.add(str(pk))
         request.session['seen_quotes'] = list(seen)
+        request.session.modified = True
